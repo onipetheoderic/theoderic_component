0e6f171fb73e3e0f8540daa8a405aa0c19b49e0f
theoderic <onipetheoderic@gmail.com>
2022-08-23 08:00:34 +0200

    fix: changed the color of the secondary button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -24,7 +24,7 @@ const Button = ({
         cursor: 'pointer',
         color: 'white',
         borderRadius: 20,
-        backgroundColor: variant === 'primary' ? 'blue' : 'gray',
+        backgroundColor: variant === 'primary' ? 'green' : 'gray',
       }}
     >
       {children}
